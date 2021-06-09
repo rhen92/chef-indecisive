02dcd8f9f10b773a1dd09c49651a2c66d1f5609e
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-09 17:27:35 -0600

    Add bold tags

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -8,12 +8,12 @@ const Recipes = ({ recipes, error }) => {
       <article>
         <h2>{randomRecipe.label}</h2>
         <img src={randomRecipe.image} alt={randomRecipe.label} />
-        <p>{randomRecipe.source}</p>
-        <p>{randomRecipe.yield}</p>
+        <p><b>Recipe Source:</b> {randomRecipe.source}</p>
+        <p><b>Serves:</b> {randomRecipe.yield}</p>
         <p><b>Ingredients:</b></p>
         <Ingredients ingredients={randomRecipe.ingredients} />
-        <p>{randomRecipe.calories}</p>
-        <p>{randomRecipe.cuisineType}</p>
+        <p><b>Calories:</b> {randomRecipe.calories}</p>
+        <p><b>Cuisine Type:</b> {randomRecipe.cuisineType}</p>
         <a href={randomRecipe.url} target="_blank">Click here to open the recipe in a new tab</a>
       </article>
     ) : <h2>Loading recipe...</h2>
