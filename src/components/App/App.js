b999dceeea60f148095e2286641d34cc730c2ac3
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-09 16:20:45 -0600

    Fix render method to remove error in console

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,56 +2,56 @@ import React, { Component } from 'react';
 import './App.css';
 import { getRecipes } from '../../api-calls';
 import Recipes from '../Recipes/Recipes';
+import NavBar from '../NavBar/NavBar';
 import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      recipes: null,
+      recipes: [],
       error: ''
     }
   }
 
-  componentDidMount = async () => {
-    try {
-      const obtainRecipes = await getRecipes();
-      const updatedRecipes = obtainRecipes.hits.reduce((obj, hit) => {
-        let recipeInfo = {
-          'label': hit.recipe.label,
-          'image': hit.recipe.image,
-          'source': hit.recipe.source,
-          'url': hit.recipe.url,
-          'yield': hit.recipe.yield,
-          'ingredients': hit.recipe.ingredientLines,
-          'calories': hit.recipe.calories,
-          'cuisineType': hit.recipe.cuisineType
-        }
-        obj.push(recipeInfo)
-        return obj;
-      }, []);
-      this.setState({recipes: updatedRecipes});
-    } catch (e) {
-      this.setState({error: 'Unable to get your recipe'})
-    }
+  componentDidMount = () => {
+    getRecipes()
+      .then(data => {
+        const updatedRecipes = data.hits.reduce((obj, hit) => {
+          let recipeInfo = {
+            'label': hit.recipe.label,
+            'image': hit.recipe.image,
+            'source': hit.recipe.source,
+            'url': hit.recipe.url,
+            'yield': hit.recipe.yield,
+            'ingredients': hit.recipe.ingredientLines,
+            'calories': hit.recipe.calories,
+            'cuisineType': hit.recipe.cuisineType
+          }
+          obj.push(recipeInfo)
+          return obj;
+        }, []);
+        this.setState({recipes: updatedRecipes});
+
+      })
+    .catch(error => this.setState({error: 'Something is wrong'}))
+
   }
 
   render() {
     return (
-      <React.Fragment>
+      <main>
       <Switch>
-      {this.state.error && <h2>{this.state.error}</h2>}
-      {!this.state.recipes && <h2>Loading recipe...</h2>}
         <Route exact path="/" render={() => {
           return (
-            <main className="App">
-              <h1>Chef Indecisive</h1>
-              <Recipes recipes={this.state.recipes} />
-            </main>
+            <section className="App">
+              <NavBar />
+              <Recipes recipes={this.state.recipes} error={this.state.error} />
+            </section>
           )
         }} />
       </Switch>
-      </React.Fragment>
+      </main>
     );
   }
 }
