65aa34a8629bae88b575a380e9c8b0e61f556f16
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-09 14:00:45 -0600

    Build out functional component for Recipes

---
+++ src/components/Recipes/Recipes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import RecipeDetails from '../RecipeDetails/RecipeDetails';
+import './Recipes.css';
+
+const Recipes = ({ recipes }) => {
+  const randomRecipe = recipes[Math.floor(Math.random() * recipes.length)];
+  const randomRecipeInfo = randomRecipe.map(recipe => {
+    return (
+      <RecipeDetails
+        name={recipe.label}
+        image={recipe.image}
+        source={recipe.source}
+        yield={recipe.yield}
+        ingredients={recipe.ingredients}
+        calories={recipe.calories}
+        cuisineType={recipe.cuisineType}
+        link={recipe.url}
+        />
+    )
+  })
+
+  return (
+    <div>
+      {randomRecipeInfo}
+    </div>
+  )
+}
+
+export default Recipes;
