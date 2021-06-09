617f4a0c6ab9ee0ed0ca9b9b46602dee8eb6f884
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-09 16:20:08 -0600

    Build out beginning of NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <header>
+      <h1>Chef Indecisive</h1>
+    </header>
+  )
+}
+
+export default NavBar;
