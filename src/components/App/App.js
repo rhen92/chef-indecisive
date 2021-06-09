e7dcfb4938b0ffdf4476ebffc4e64bba6f4f004a
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-09 13:20:59 -0600

    Add componentDidMount method and clean api call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,47 @@
+import React, { Component } from 'react';
 import './App.css';
+import { getRecipes } from '../../api-calls';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      recipes: null,
+      error: ''
+    }
+  }
+
+  componentDidMount = async () => {
+    try {
+      const obtainRecipes = await getRecipes();
+      const updatedRecipes = obtainRecipes.hits.reduce((obj, hit) => {
+        let recipeInfo = {
+          'label': hit.recipe.label,
+          'image': hit.recipe.image,
+          'source': hit.recipe.source,
+          'url': hit.recipe.url,
+          'yield': hit.recipe.yield,
+          'ingredients': hit.recipe.ingredientLines,
+          'calories': hit.recipe.calories,
+          'cuisineType': hit.recipe.cuisineType
+        }
+        obj.push(recipeInfo)
+        return obj;
+      }, []);
+      this.setState({recipes: updatedRecipes});
+      console.log(this.state.recipes)
+    } catch (e) {
+      this.setState({error: 'Unable to get your recipe'})
+    }
+  }
+
+  render() {
+    return (
+      <main className="App">
       <h1>Hello</h1>
-    </div>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
