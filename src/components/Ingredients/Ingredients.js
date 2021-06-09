926ea8d4d41ea0323731bb7542b6bcb21dbc31c5
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-09 17:23:31 -0600

    Make Ingredients functional component

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,1 +1,18 @@
+import React from 'react';
 
+const Ingredients = ({ ingredients }) => {
+  const listOfIngredients = ingredients.map(ingredient => {
+    return (
+      <ul>
+      <li>{ingredient}</li>
+      </ul>
+    )
+  })
+  return (
+    <>
+      {listOfIngredients}
+    </>
+  )
+}
+
+export default Ingredients;
