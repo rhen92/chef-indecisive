23fecc8b01d291897c71d70623dd5920f1a48a98
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-10 13:12:08 -0600

    Add Link to button to change page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './NavBar.css';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <header>
       <h1>Chef Indecisive</h1>
+      <Link to={'/favoriteRecipes'}>
       <button className='button-styling'>Head to Favorites</button>
+      </Link>
     </header>
   )
 }
