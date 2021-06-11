63c1599f042db012280d6156150122d267c660e3
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-11 10:25:16 -0600

    Change how ingredients are shown

--- src/components/FavoriteRecipeCard/FavoriteRecipeCard.js
+++ src/components/FavoriteRecipeCard/FavoriteRecipeCard.js
@@ -10,8 +10,7 @@ const FavoriteRecipeCard = ({ label, image, source, serves, ingredients, calorie
     <img className='favorite-food-pic' src={image} alt={label} />
     <p><b>Recipe Source:</b> {source}</p>
     <p><b>Serves:</b> {serves}</p>
-    <p><b>Ingredients:</b></p>
-    <Ingredients ingredients={ingredients} />
+    <p><b>Ingredients:</b> {ingredients}</p>
     <p><b>Calories:</b> {Math.round(calories)}</p>
     <p><b>Cuisine Type:</b> {cuisineType}</p>
     <a href={url} target='_blank' rel='noreferrer'>Click here to open the recipe in a new tab</a>
