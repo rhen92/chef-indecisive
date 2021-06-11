eee72bb0cb28e32f678352af116f5621a3a76a31
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-11 10:18:44 -0600

    Add classNames and import statement

--- src/components/FavoriteRecipeCard/FavoriteRecipeCard.js
+++ src/components/FavoriteRecipeCard/FavoriteRecipeCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './FavoriteRecipeCard.css';
 import Ingredients from '../Ingredients/Ingredients';
 
 const FavoriteRecipeCard = ({ label, image, source, serves, ingredients, calories, cuisineType, url, deleteRecipe }) => {
   return (
-    <article>
+    <article className='favorite-card'>
     <i className='fas fa-times-circle' onClick={() => deleteRecipe(label)} />
     <h2>{label}</h2>
-    <img className='food-pic' src={image} alt={label} />
+    <img className='favorite-food-pic' src={image} alt={label} />
     <p><b>Recipe Source:</b> {source}</p>
     <p><b>Serves:</b> {serves}</p>
     <p><b>Ingredients:</b></p>
