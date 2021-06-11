e9d9079acbba3194ec058a7b70717cab1d948e97
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-11 10:30:58 -0600

    Remove import statement and join ingredients with comma

--- src/components/FavoriteRecipeCard/FavoriteRecipeCard.js
+++ src/components/FavoriteRecipeCard/FavoriteRecipeCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './FavoriteRecipeCard.css';
-import Ingredients from '../Ingredients/Ingredients';
 
 const FavoriteRecipeCard = ({ label, image, source, serves, ingredients, calories, cuisineType, url, deleteRecipe }) => {
   return (
@@ -10,7 +9,7 @@ const FavoriteRecipeCard = ({ label, image, source, serves, ingredients, calorie
     <img className='favorite-food-pic' src={image} alt={label} />
     <p><b>Recipe Source:</b> {source}</p>
     <p><b>Serves:</b> {serves}</p>
-    <p><b>Ingredients:</b> {ingredients}</p>
+    <p><b>Ingredients:</b> {ingredients.join(',')}</p>
     <p><b>Calories:</b> {Math.round(calories)}</p>
     <p><b>Cuisine Type:</b> {cuisineType}</p>
     <a href={url} target='_blank' rel='noreferrer'>Click here to open the recipe in a new tab</a>
