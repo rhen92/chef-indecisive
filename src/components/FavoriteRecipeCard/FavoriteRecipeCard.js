d85acdf6aabba0f2c60d5aceb7be19bc28cac081
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-11 13:21:35 -0600

    Organize tags a different way

--- src/components/FavoriteRecipeCard/FavoriteRecipeCard.js
+++ src/components/FavoriteRecipeCard/FavoriteRecipeCard.js
@@ -4,15 +4,17 @@ import './FavoriteRecipeCard.css';
 const FavoriteRecipeCard = ({ label, image, source, serves, ingredients, calories, cuisineType, url, deleteRecipe }) => {
   return (
     <article className='favorite-card'>
-    <i className='fas fa-times-circle' onClick={() => deleteRecipe(label)} />
-    <h2>{label}</h2>
-    <img className='favorite-food-pic' src={image} alt={label} />
-    <p><b>Recipe Source:</b> {source}</p>
-    <p><b>Serves:</b> {serves}</p>
-    <p><b>Ingredients:</b> {ingredients.join(',')}</p>
-    <p><b>Calories:</b> {Math.round(calories)}</p>
-    <p><b>Cuisine Type:</b> {cuisineType}</p>
-    <a href={url} target='_blank' rel='noreferrer'>Click here to open the recipe in a new tab</a>
+      <i className='fas fa-times-circle' onClick={() => deleteRecipe(label)} />
+      <div>
+        <img className='favorite-food-pic' src={image} alt={label} />
+      </div>
+        <h2>{label}</h2>
+        <p><b>Recipe Source:</b> {source}</p>
+        <p><b>Serves:</b> {serves}</p>
+        <p><b>Ingredients:</b> {ingredients.join(',')}</p>
+        <p><b>Calories:</b> {Math.round(calories)}</p>
+        <p><b>Cuisine Type:</b> {cuisineType}</p>
+      <a href={url} target='_blank' rel='noreferrer'>Click here to open the recipe in a new tab</a>
     </article>
   )
 }
