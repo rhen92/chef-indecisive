82124c21a86b2527a6f4860aac86ff876a48c387
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-12 09:01:10 -0600

    Add allRecipes.json file to fixtures

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,5 @@
 Cypress.Commands.add('getRecipes', () => {
-  cy.fixture('../fixtures/allrecipes.json')
+  cy.fixture('../fixtures/allRecipes.json')
     .then(json => {
       cy.intercept('https://api.edamam.com/search?q=vegetarian&app_id=3eafd667&app_key=0be9bcadcee0d343f2134be2a669f875&from=0&to=100&mealType=dinner', json)
     })
