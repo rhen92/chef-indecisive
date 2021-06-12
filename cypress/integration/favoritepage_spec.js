c92de6e2ee589164ae304f0138487d6f36347067
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-12 15:59:43 -0600

    Add test to check url change

---
+++ cypress/integration/favoritepage_spec.js
@@ -0,0 +1,12 @@
+describe('Favorite Page', () => {
+  beforeEach(() => {
+    cy.interceptRecipes()
+      .visit('http://localhost:3000')
+  })
+
+  it('should show url change when going to Favorite Page', () => {
+    cy.get('button').eq(0).click()
+      .url().should('eq', 'http://localhost:3000/favoriteRecipes')
+      .get('button').contains('Go back to Home')
+  })
+})
