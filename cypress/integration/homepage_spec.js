cf4fb666c6d1b2484e4cbbcfb8940c9ec4a4bc7c
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-12 14:57:40 -0600

    Add test to check show different recipe button functionality

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -12,4 +12,17 @@ describe('Home Page', () => {
   it('should show loading recipe when fetching data', () => {
     cy.get('h2').contains('Loading recipe...')
   })
+
+  it('should show a different recipe show different recipe button is clicked', () => {
+    cy.wait(1000)
+      .get('h2').invoke('text')
+      .then((text1) => {
+        cy.get('button').eq(1).click()
+          .get('h2').invoke('text')
+          .should((text2) => {
+            expect(text1).not.to.eq(text2)
+          })
+      })
+  })
+
 })
